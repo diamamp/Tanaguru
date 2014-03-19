2d9d525d8796372405d746db1b8ecd9d0205c8ff
skhattab <skhattab@OCEANE-FIXE25.oceane.local>
2014-03-19 15:47:18 +0100

    Test

--- web-app/tgol-web-app/src/test/java/org/opens/tgol/controller/AuditSetUpControllerTest.java
+++ web-app/tgol-web-app/src/test/java/org/opens/tgol/controller/AuditSetUpControllerTest.java
@@ -22,8 +22,10 @@
 package org.opens.tgol.controller;
 
 import java.util.*;
+
 import junit.framework.TestCase;
 import static org.easymock.EasyMock.*;
+
 import org.opens.tanaguru.entity.parameterization.Parameter;
 import org.opens.tanaguru.entity.parameterization.ParameterElement;
 import org.opens.tanaguru.entity.service.parameterization.ParameterElementDataService;
@@ -62,7 +64,8 @@ public class AuditSetUpControllerTest extends TestCase {
     private AuditSetUpController instance;
 
     Authentication mockAuthentication;
-    AuthenticationDetails mockAuthenticationDetails;
+    @SuppressWarnings("deprecation")
+	AuthenticationDetails mockAuthenticationDetails;
     ActDataService mockActDataService;
     User mockUser;
     UserDataService mockUserDataService;
